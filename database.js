2aab6df93a8e32f316d1a91cf0f39df064feef0b
pntsunts <50695155+pntsunts@users.noreply.github.com>
2021-05-17 15:46:21 +0200

    Delete database.js

--- database.js
+++
@@ -1,9 +0,0 @@
-const { Sequelize } = require('sequelize');
-
-const sequelize = new Sequelize('test', 'user', 'password', {
-    dialect: 'sqlite',
-    host: './dev.sqlite'
-})
-
-module.exports = sequelize;
-
